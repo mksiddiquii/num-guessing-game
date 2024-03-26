54130fcba3f120b069f0f381231fe932594050e6
mksiddiqui <mksiddiquii@hotmail.com>
2024-03-26 15:46:40 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+import chlak from "chalk";
+console.log(chlak.yellow("You have Three Chances to Win The Game!"));
+//let randomNumber =Math.floor(Math.random() * 10)
+let randomNumber = 7;
+for (let i = 0; i < 3; i++) {
+    const callNumber = await inquirer.prompt([
+        {
+            message: chlak.blue("Please guess the Any Number Between 1 to 10 "),
+            type: "number",
+            name: "firstGuess",
+        },
+    ]);
+    console.log(callNumber.firstGuess === randomNumber ? chlak.green("You win The Game") : chlak.red("Try Again"));
+}
